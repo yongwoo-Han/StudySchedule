6bac62eb1ef8f4d654f4cfcac98280abb090b8c3
yongwoo-Han <ywhan@ibizsoftware.net>
2019-03-23 12:59:32 +0900

    init Commit

---
+++ src/main/java/com/example/demo/controller/StudyScheduleController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/")
+public class StudyScheduleController {
+
+	@RequestMapping(value = "index")
+	public ModelAndView hello() {
+		return new ModelAndView();
+	}
+}
